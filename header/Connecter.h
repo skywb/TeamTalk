5051c88b66b43ec27cc12d7f478b3359cf9b4f48
skywb <sonky7@163.com>
2019-03-29 19:53:45 +0800

    add Connecter

---
+++ header/Connecter.h
@@ -0,0 +1,70 @@
+#ifndef CONNECTER_H_TCUVAPLY
+#define CONNECTER_H_TCUVAPLY
+
+#include "Addresser.h"
+#include "sockUtil.h"
+#include <memory>
+#include <mutex>
+#include <memory>
+#include <cstring>
+#include <vector>
+
+namespace BlueCat {
+
+	class Buffer
+	{
+	public:
+		Buffer () : buf(nullptr), _length(0) {}
+		Buffer (const char* str) {
+			_length = strlen(str);
+			if(_length > MSGMAXSIZE) {
+				//log(error);
+				buf = nullptr;
+			} else {
+				buf = new char[MSGMAXSIZE];
+				::strncpy(buf, str, _length);
+			}
+		}
+		virtual ~Buffer () {
+			delete[] buf;
+		}
+
+		bool addMsg(const char* msg);
+		const char* c_str() {
+			return buf;
+		}
+	
+	private:
+		static const unsigned int MSGMAXSIZE = BUFSIZ;
+		size_t _length;
+		char * buf;
+	};
+
+
+
+	class Connecter
+	{
+	public:
+		Connecter(int sockfd) : _sockfd(sockfd), connected(true) {
+
+		}
+		virtual ~Connecter();
+
+		virtual bool send(char* msg);
+		virtual int recive(char* buf);
+		virtual bool isConnected() {
+			std::lock_guard<std::mutex> lock(_mutex);
+			return connected; 
+		}
+	
+	protected:
+		int _sockfd;
+		std::vector<Buffer> _bufque;
+		std::mutex _mutex;
+		bool connected;
+	};
+
+
+}
+
+#endif /* end of include guard: CONNECTER_H_TCUVAPLY */
---
+++ source/Connecter.cpp
@@ -0,0 +1,58 @@
+#include "Connecter.h"
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+namespace BlueCat {
+
+	bool Buffer::addMsg(const char* msg) {
+		size_t len = strlen(msg);
+		if(len > MSGMAXSIZE) {
+			//log(msglength greate the MSGMAXSIZE)
+			return false;
+		}
+		if(buf == nullptr) {
+			_length = len;
+			buf = new char[MSGMAXSIZE];
+			::strncpy(buf, msg, _length);
+		} else {
+			if(_length + len > MSGMAXSIZE) {
+				//log()
+				return false;
+			}
+			::strncpy(buf+_length, msg, len);	
+			_length += len;
+		}
+		return true;
+	}
+
+
+	Connecter::~Connecter() {
+		close(_sockfd);
+	}
+
+	bool Connecter::send(char* msg) {
+		std::lock_guard<std::mutex> lock(_mutex);
+		int len = strlen(msg);
+		int re = write(_sockfd, msg, len);
+
+		if(re == -1) {
+			//log(write eror)
+			return false;
+		}
+		else if(re < len) {
+			_bufque.push_back(Buffer(msg+re));
+			/* TODO: 添加可写事件 */
+			return true;
+		}
+
+		return true;
+	}
+	int Connecter::recive(char* buf) {
+		std::lock_guard<std::mutex> lock(_mutex);
+
+		int n =  read(_sockfd, buf, BUFSIZ);
+		if(n == 0) connected = false;
+		return n;
+	}
+}
