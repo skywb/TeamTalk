78e8fa1cc08b48745290214cd4ed1810c1992e9a
skywb <sonky7@163.com>
2019-04-27 17:44:00 +0800

    add setting

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,77 @@
+import os
+import ycm_core
+flags = [
+'-Wall',
+'-Wextra',
+#'-Werror',
+'-fexceptions',
+'-DNDEBUG',
+'-std=c++11',
+'-x',
+'c++',
+'-isystem',
+'/usr/include/c++/7',
+'-isystem',
+'/usr/include/x86_64-linux-gnu',
+'-isystem',
+'/usr/local/include',
+'-isystem',
+'/usr/include',
+'-isystem',
+'./',
+
+
+]
+
+
+compilation_database_folder = ''
+
+if os.path.exists( compilation_database_folder ):
+  database = ycm_core.CompilationDatabase( compilation_database_folder )
+else:
+  database = None
+
+SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm']
+
+def DirectoryOfThisScript():
+  return os.path.dirname( os.path.abspath( __file__ ) )
+
+
+def IsHeaderFile( filename ):
+  extension = os.path.splitext( filename )[ 1 ]
+  return extension in [ '.h', '.hxx', '.hpp', '.hh' ]
+
+
+def GetCompilationInfoForFile( filename ):
+  if IsHeaderFile( filename ):
+    basename = os.path.splitext( filename )[ 0 ]
+    for extension in SOURCE_EXTENSIONS:
+      replacement_file = basename + extension
+      if os.path.exists( replacement_file ):
+        compilation_info = database.GetCompilationInfoForFile(
+          replacement_file )
+        if compilation_info.compiler_flags_:
+          return compilation_info
+    return None
+  return database.GetCompilationInfoForFile( filename )
+
+
+# This is the entry point; this function is called by ycmd to produce flags for
+# a file.
+def FlagsForFile( filename, **kwargs ):
+  if not database:
+    return {
+      'flags': flags,
+      'include_paths_relative_to_dir': DirectoryOfThisScript()
+    }
+
+  compilation_info = GetCompilationInfoForFile( filename )
+  if not compilation_info:
+    return None
+
+  # Bear in mind that compilation_info.compiler_flags_ does NOT return a
+  # python list, but a "list-like" StringVec object.
+  return {
+    'flags': list( compilation_info.compiler_flags_ ),
+    'include_paths_relative_to_dir': compilation_info.compiler_working_dir_
+  }
---
+++ workspace.vim
@@ -0,0 +1,9 @@
+
+"开启tags补全引擎， 只支持ctags， 必须使用 --fields=——l选项
+let g:ycm_collect_identifiers_from_tags_files = 0
+
+
+command! Todo Ack! 'TODO|FIXME|CHANGED|BUG|HACK'
+
+set tags=./tags
+
